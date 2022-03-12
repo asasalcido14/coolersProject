a890bd23f19120caf5152a4c6c90770017dca623
asasalcido14 <asasalcido14@email.arizona.edu>
2022-03-12 00:11:50 -0700

    Fixed error
    added ) to the forEach expression. got another error on chrom now

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
-//
-
-
 
 //Global selections and variables
-
-
+const colorDivs = document.querySelectorAll('.color');
+const generateBtn = document.querySelector('.generate');
+const sliders = document.querySelectorAll('input[type= "range"]');
+const currentHexes = document.querySelectorAll('.color h2');
+let initialColors;
 
 
 // functions
+
+//Color Generateor
 function generateHex(){
-    const letters = '#0123456789ABCDEF';
-    let hash = '#';
-    for(let i = 0; i<6; i++) {
-        hash+= letters[Math.floor(Math.random() * 16)];
-    }
-    return hash;
+    const hexColor = chrom.random();
+    return hexColor;
 }
 
-let randomHex = generateHex();
-console.log(randomHex);
+function randomColors() {
+    colorDivs.forEach((div, index) => {
+        const hexText = div.children[0];
+        const randomColor = generateHex();
+
+        //ADD THE COLOR TO THE BG
+        div.style.backgroundColor = randomColor;
+        hexText.innerText = randomColor;
+    });
+}
+randomColors();
